c6533191ee4db61131498f36a6df3900b3fbbbbd
KostyaVLG <dka1977@yandex.ru>
2023-05-21 10:19:46 +0300

    Dobavil 4 zadachi

---
+++ ZADACHA_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Здравструйте. Эта программа, покажет Вам, какое число большее, а какое меньшее.");
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine()); 
+
+if (firstNumber > secondNumber) // Равно ли 25 числу 5 в квадрате?
+{
+    // Да, истина
+    Console.WriteLine("Первое число " + firstNumber + " является максимальным по отношению к второму числу " + secondNumber);
+}
+else // Нет
+{
+    Console.WriteLine("Первое число " + firstNumber + " НЕ является максимальным по отношению к второму числу " + secondNumber);
+}
---
+++ ZADACHA_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Здравструйте. программа покажет вам максимальное из трех заданных чисел.");
+
+Console.Write("Введите первое число: ");
+int Number_1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Введите второе число: ");
+int Number_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int Number_3= Convert.ToInt32(Console.ReadLine());
+
+int max = Number_1;
+if (Number_2 > max)
+{
+max = Number_2;
+}
+if (Number_3 > max)
+{
+max = Number_3;
+}
+Console.WriteLine("Наибольшее из введённых чисел = " + max);
